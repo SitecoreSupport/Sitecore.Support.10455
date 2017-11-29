702a2867ef0bce4ed8b2e43cf05dad309384310a
AntonKechashin <atk@sitecore.net>
2017-11-29 21:47:34 +0200

    Initial commit - added a check if the context site is SXA site

---
+++ src/Sitecore.Support.10455/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.10455")]
+[assembly: AssemblyProduct("Sitecore.Support.10455")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.10455/XA/Feature/SiteMetadata/Pipelines/GetRobotsContent/AppendSitemapUrl.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Support.XA.Feature.SiteMetadata.Pipelines.GetRobotsContent
+{
+  using Sitecore.XA.Feature.SiteMetadata.Pipelines.GetRobotsContent;
+  using Sitecore.XA.Foundation.Multisite.Extensions;
+
+  public class AppendSitemapUrl : Sitecore.XA.Feature.SiteMetadata.Pipelines.GetRobotsContent.AppendSitemapUrl
+  {
+    public override void Process(GetRobotsContentArgs args)
+    {
+      #region Added code
+      if (Sitecore.Context.Site.IsSxaSite())
+      {
+        base.Process(args);
+      } 
+      #endregion
+    }
+  }
+}
