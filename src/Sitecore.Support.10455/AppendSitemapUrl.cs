a69f8eba3b1e89b1ae048a7120ff811c66c7620f
DK\whc <whc@sitecore.net>
2018-08-02 11:21:27 +0800

    Fix robots.txt for sc8.1.3 sxa1.4

---
+++ src/Sitecore.Support.10455/AppendSitemapUrl.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.XA.Feature.SiteMetadata.Pipelines.GetRobotsContent;
+using Sitecore.XA.Foundation.Multisite.Extensions;
+
+namespace Sitecore.Support.XA.Feature.SiteMetadata.Pipelines.GetRobotsContent
+{
+  public class AppendSitemapUrl : Sitecore.XA.Feature.SiteMetadata.Pipelines.GetRobotsContent.AppendSitemapUrl
+  {
+    public virtual void Process(GetRobotsContentArgs args)
+    {
+      if (!SiteExtensions.IsSxaSite(Context.Site))
+        return;
+      base.Process(args);
+    }
+  }
+}
